076de479de6ca5e247461abf62a862c88af33e6f
Joseph Romano <jdromano2@gmail.com>
2019-08-15 12:15:10 -0400

    Rework phenotypes and conditions

--- build/add_individuals.py
+++ build/add_individuals.py
@@ -2,8 +2,27 @@
 
 import rdflib
 
-# Add hetionet entities:
+####
+# Add general biomedical data
+#### 
+
+# Add hetionet nodes:
 # 1. Genes
 # 2. Drugs ("Compounds")
 # 3. Diseases
 # 4. Anatomic entities ("Anatomy")
+
+
+
+# Add hetionet relationships:
+# 1. chemicalBindsGene ("BINDS_CbG")
+# 2. chemicalCausesEffect ("CAUSES_CcSE")
+# 3. 
+
+
+# Add hetionet property keys:
+# 1.
+
+####
+# Add toxicology data
+####

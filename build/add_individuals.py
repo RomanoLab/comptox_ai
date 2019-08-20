7fe814c17de72f138f1f326a015b72b10b4e8022
Joseph D. Romano <jdromano2@gmail.com>
2019-08-19 23:47:21 -0400

    Continue to add hetionet individuals

--- build/add_individuals.py
+++ build/add_individuals.py
@@ -2,16 +2,31 @@
 
 from owlready2 import get_ontology
 import pandas as pd
+from tqdm import tqdm
 
 import ipdb, traceback, sys
 
 ONTOLOGY_FNAME = "../comptox.rdf"
 ONTOLOGY_POPULATED_FNAME = "../comptox_populated.rdf"
 
+ONTOLOGY_IRI = "http://jdr.bio/ontologies/comptox.owl#"
+ONTOLOGY_POPULATED_IRI = 'http://jdr.bio/ontologies/comptox-full.owl#'
 
-def summarize_graph(grph):
-    print("Entities in graph: {0}".format(len(grph)))
-    print("Number of nodes: {0}".format(len(grph.all_nodes())))
+
+def make_safe_property_label(label):
+    """Convert the label ("name") of a property to a safe format.
+
+    We follow the convention that only class names can begin with an uppercase letter.
+    This can be explained using the following example: One of the 'pathways' in Hetionet
+    is named "Disease", but Disease is already a class in the ontology. Therefore, there
+    is no way to distinguish between these two entities in Python.
+
+    This may have to be reevaluated later, if lowercasing entity names is leading to
+    more problems down the line.
+    """
+    return label.replace(" ", "_").lower()
+
+print("Reading objects (ontology, nodes, etc.) into memory...")
 
 # Load graph
 ont = get_ontology(ONTOLOGY_FNAME).load()
@@ -26,36 +41,51 @@ ont = get_ontology(ONTOLOGY_FNAME).load()
 # 3. Diseases
 # 4. Anatomic entities ("Anatomy")
 hetio_nodes = pd.read_csv("../data/hetionet/hetionet-v1.0-nodes.tsv", sep="\t")
-for idx,n in hetio_nodes.iterrows():
+print("Adding Hetionet nodes as ontology individuals...")
+for idx,n in tqdm(hetio_nodes.iterrows(), total=len(hetio_nodes)):
     nodetype = n[2]
+    nm = make_safe_property_label(n[1])
+    
     if nodetype=="Anatomy":
-        ont.StructuralEntity(n[1].replace(" ","_"), xrefUberon=n[0].split("::")[-1])
+        ont.StructuralEntity(nm, xrefUberon=n[0].split("::")[-1])
     elif nodetype=="BiologicalProcess":
-        continue
+        ipdb.set_trace()
+        print()
     elif nodetype=="CellularComponent":
         continue
     elif nodetype=="Compound":
         # NOTE: "Compounds" in hetionet are DrugBank entities,
         # which correspond most closely to "Chemical"s in comptox
-        name = n[1]
         drugbank_id = n[0].split("::")[-1]
-        ont.Chemical(name.replace(" ","_"), xrefDrugbank=drugbank_id, chemicalIsDrug=True, chemicalName=name)
+        ont.Chemical(nm, xrefDrugbank=drugbank_id, chemicalIsDrug=True)
     elif nodetype=="Disease":
         continue
     elif nodetype=="Gene":
-        symbol = n[1]
         ncbi_gene = n[0].split("::")[-1]
-        ont.Gene(symbol, geneSymbol=symbol, xrefNcbiGene=ncbi_gene)
+        ont.Gene(nm, geneSymbol=n[1], xrefNcbiGene=ncbi_gene)
     elif nodetype=="MolecularFunction":
         continue
     elif nodetype=="Pathway":
-        continue
+        pass  # NOTE: Need to assess quality of "pathway" entities! E.g., why is "Immune System"
+              # considered a pathway? And what do the identifiers mean (e.g., PC7_4688)???
+        # try:
+        #     hetio_id = n[0].split("::")[-1]
+        #     p = ont.Pathway(nm)
+        #     p.xrefUnknownPathway.append(hetio_id)  # NOTE: can't assign to non-functional property during instantiation
+        # except:
+        #     ipdb.set_trace()
+        #     print("Looks like something went wrong")
     elif nodetype=="Pharmacologic Class":
+        # TODO: Add drug class to ontology
         continue
     elif nodetype=="Side Effect":
-        continue
+        # These nodes were sourced from SIDER, which - by definition - describes drug adverse effects.
+        ont.AdverseEffect(nm, xrefUmlsCUI=n[0].split("::")[-1])
     elif nodetype=="Symptom":
-        continue
+        # NOTE: May need to revise knowledge model if symptoms can map to multiple MeSH terms (i.e.,
+        # if the DbXref is not functional)
+        ont.Phenotype(nm, xrefMeSH=n[0].split("::")[-1])
+del(hetio_nodes)
 
 # Add hetionet relationships:
 # 1. chemicalBindsGene ("BINDS_CbG")
@@ -63,7 +93,9 @@ for idx,n in hetio_nodes.iterrows():
 # 3. diseaseRegulatesGeneOther ("ASSOCIATES_DaG")
 # 4. diseaseUpregulatesGene ("UPREGULATES_DuG")
 # 5. diseaseDownregulatesGene ("DOWNREGULATES_DdG")
-
+# hetio_rels = pd.read_csv("../data/hetionet/hetionet-v1.0-edges.sif", sep="\t")
+# for idx,r in tqdm(hetio_rels.iterrows(), total=len(hetio_rels)):
+#     continue
 
 # Add hetionet property keys:
 # 1.
@@ -82,10 +114,18 @@ for idx,n in hetio_nodes.iterrows():
 # 2. Specify (if not already done) key initiating events; LINK TO CHEMICALS
 # 3. Link KEs to adverse outcomes (either link to AEs and then to Disease/Phenotype or remove AEs entirely)
 
+# Some final housekeeping
+ont.name = 'comptox-full'  # Name needs to be different from unpopulated ontology
+ont.base_iri = ONTOLOGY_POPULATED_IRI
+
 # Write modified graph (TO DIFFERENT FILE)
+print("Writing populated ontology to disk (as RDF-formatted XML)...")
 try:
     ont.save(file=ONTOLOGY_POPULATED_FNAME, format="rdfxml")
+    pass
 except TypeError:
     extype, value, tb = sys.exc_info()
+    print("Uh oh, something went wrong when serializing the populated ontology to disk. Entering debug mode...")
     traceback.print_exc()
     ipdb.post_mortem(tb)
+

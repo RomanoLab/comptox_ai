5c2c08d55c1db209d542aeb66146c524279c1a37
Joseph Romano <jdromano2@gmail.com>
2019-08-23 15:05:18 -0400

    Begin to add AOP Wiki data

--- build/3_merge_aopwiki.py
+++ build/3_merge_aopwiki.py
@@ -16,3 +16,35 @@ ONTOLOGY_POPULATED_IRI = 'http://jdr.bio/ontologies/comptox-full.owl#'
 OWL = get_ontology("http://www.w3.org/2002/07/owl#")
 
 ont = get_ontology("../comptox_populated_linked.rdf").load()
+
+kes = pd.read_csv("../data/aop_ke_mie_ao.tsv",
+                  sep="\t",
+                  header=None,
+                  names=['aop_id',
+                         'key_event_id',
+                         'key_event_type',
+                         'key_event_name'])
+
+kers = pd.read_csv("../data/aop_ke_ker.tsv",
+                   sep="\t",
+                   header=None,
+                   names=['aop_id',
+                          'upstream_event_id',
+                          'downstream_event_id',
+                          'relationship_id',
+                          'direct_or_indirect',
+                          'evidence',
+                          'quantitative_understanding'])
+
+ke_components = pd.read_csv("../data/aop_ke_ec.tsv",
+                            sep="\t",
+                            header=None,
+                            names=['aop_id',
+                                   'key_event_id',
+                                   'action',
+                                   'object_source',
+                                   'object_ontology_id',
+                                   'object_term',
+                                   'process_source',
+                                   'process_ontology_id',
+                                   'process_term'])

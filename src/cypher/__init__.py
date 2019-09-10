7c211db81a757a6c2681b6c9ad6d032855d94054
Joseph Romano <jdromano2@gmail.com>
2019-09-10 09:09:34 -0400

    Connect ComptoxOntology to Neo4j
    Former-commit-id: 134c101d46d524403a6db54a5d8d86112fd02627

---
+++ src/cypher/__init__.py
@@ -0,0 +1,6 @@
+"""
+This is a library of all Cypher query templates used in the Comptox
+Ontology code.
+"""
+
+from . import queries
---
+++ src/cypher/queries.py
@@ -0,0 +1,8 @@
+MIE_DISEASE_PATH = """
+MATCH
+	(d:ns0__Disease {{ns0__commonName: '{}'}}),
+	(m:ns0__MolecularInitiatingEvent {{ns0__keyEventID: '{}'}}),
+	p=shortestPath((d)-[*]-(m))
+WHERE length(p)>1
+RETURN p;
+"""[1:-1]

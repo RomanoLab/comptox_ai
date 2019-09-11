24da9e0319f5967a53b351bf1c894d4e7fbcc679
Joseph Romano <jdromano2@gmail.com>
2019-09-11 11:42:32 -0400

    Rename package source directory for autodoc compatibility
    Former-commit-id: c0dd48628a1c2259849b8de198156081f6a73262

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", 'r') as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    name="comptox_ontology",
+    version="0.0.1a",
+    author="Joseph D. Romano, PhD",
+    author_email="joseph.romano@pennmedicine.upenn.edu",
+    description="An ontology and knowledge base to support discovery in computational toxicology",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jdromano2/comptox-ontology",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix"
+    ],
+    python_requires='>=3.7',
+)

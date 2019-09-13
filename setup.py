e7f95ab5cf5bfddf94c9efb3096a3ea7e9f742d8
Joseph Romano <jdromano2@gmail.com>
2019-09-13 13:11:14 -0400

    Fix bugs from project rename
    Former-commit-id: 9ac604c81dfced87a5fc0ad644937cc68f17a176

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", 'r') as fp:
     long_description = fp.read()
 
 setuptools.setup(
-    name="comptox_ontology",
+    name="comptox_ai",
     version="0.0.1a",
     author="Joseph D. Romano, PhD",
     author_email="joseph.romano@pennmedicine.upenn.edu",
     description="An ontology and knowledge base to support discovery in computational toxicology",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jdromano2/comptox-ontology",
+    url="https://github.com/jdromano2/comptox_ai",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",

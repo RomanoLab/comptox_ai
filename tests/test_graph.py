4504bc0e745d162939e70ee9124bd196d92c3a14
Joseph Romano <jdromano2@gmail.com>
2019-09-23 14:05:47 -0400

    Add default config file (with stupidly basic neo4j auth defaults)

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -2,8 +2,12 @@ import comptox_ai
 import neo4j
 
 import pytest
+import os, sys
 
-CONFIG_FILE = 'CONFIG.cfg'
+if os.path.exists('CONFIG.cfg'):
+    CONFIG_FILE = 'CONFIG.cfg'
+else:
+    CONFIG_FILE = 'CONFIG-default.cfg'
 
 class TestGraph(object):
     def test_does_create_bolt_driver(self):

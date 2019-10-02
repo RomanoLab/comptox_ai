4ae8369a93aae3e5e6d0d3f8d9288904c248629a
Joseph Romano <jdromano2@gmail.com>
2019-10-02 15:00:10 -0400

    Tidy up graph.py somewhat

--- comptox_ai/graph.py
+++ comptox_ai/graph.py
@@ -1,5 +1,6 @@
 import numpy as np
 import nxneo4j
+import neo4j
 import networkx as nx
 
 # for Spinner
@@ -11,7 +12,7 @@ from .cypher import queries
 
 from .utils import execute_cypher_transaction
 
-#TODO: Move!!!
+
 class Spinner(object):
     """See https://stackoverflow.com/a/39504463/1730417
     """
@@ -34,7 +35,7 @@ class Spinner(object):
             time.sleep(self.delay)
             sys.stdout.write('\b')
             sys.stdout.flush()
-    
+
     def __enter__(self):
         self.busy = True
         threading.Thread(target=self.spinner_task).start()
@@ -44,6 +45,7 @@ class Spinner(object):
         time.sleep(self.delay)
         if exception is not None:
             return False
+
 
 class Graph:
     """
@@ -58,7 +60,6 @@ class Graph:
 
         self.driver = driver
         self.test_driver
-        
 
         # If node_labels is the empty list, we don't filter on node type
         self.node_mask = kwargs.get("node_mask", [])
@@ -88,7 +89,7 @@ class Graph:
     def open_connection(self,
                         username,
                         password,
-                        uri = "bolt://localhost:7687"):
+                        uri="bolt://localhost:7687"):
         """Manually establish a connection between `self.graph` and a
         Neo4j graph database.
         """
@@ -110,23 +111,26 @@ class Graph:
         database currently exists and is active.
         """
         if not self.driver_connected:
-            raise RuntimeError("Attempted to query Neo4j without an active database connection")
+            raise RuntimeError("Attempted to query Neo4j without an active \
+                                database connection")
         return True
 
     # NODE RETRIEVAL METHODS
 
     def get_node_degrees(self, node_type=None):
         """Retrieve a list of URIs and their corresponding degrees.
-        
+
         Parameters
         ----------
         node_type : str, optional
-            Ontology class corresponding to the desired node type, by default None.
-        
+            Ontology class corresponding to the desired node type, by default
+            None.
+
         Returns
         -------
         list of (str, int)
-            Each returned element is a tuple containing a node's URI and that node's degree.
+            Each returned element is a tuple containing a node's URI and that 
+            node's degree.
         """
         if node_type is None:
             self.template = queries.FETCH_ALL_NODE_DEGREES
@@ -144,18 +148,18 @@ class Graph:
 
     def fetch_node_by_uri(self, uri):
         """Retrieve a node from Neo4j matching the given URI.
-        
+
         Parameters
         ----------
         uri : str
             URI of the node to fetch.
-        
+
         Returns
         -------
         neo4j.Record
             Node corresponding to the provided URI.
         """
-        if uri == None:
+        if uri is None:
             print("No URI given -- aborting")
         else:
             self.template = queries.FETCH_INDIVIDUAL_NODE_BY_URI
@@ -168,7 +172,6 @@ class Graph:
             else:
                 assert len(query_response) == 1
                 return query_response[0]
-
 
     def fetch_nodes_by_label(self, label):
         """
@@ -196,9 +199,10 @@ class Graph:
             return(query_response)
 
     def fetch_neighbors_by_uri(self, uri):
-        """Fetch nodes corresponding to neighbors of a node represented by a given URI.
+        """Fetch nodes corresponding to neighbors of a node represented by a
+        given URI.
         """
-        if uri == None:
+        if uri is None:
             print("No URI given -- aborting")
         else:
             self.template = queries.FETCH_NEIGHBORS_BY_URI
@@ -223,48 +227,38 @@ class Graph:
         -------
         networkx.graph
         """
-        # NOTE: nxneo4j actually seems to be useless!!!
-        # We use nxneo4j to eliminate some of the implementation work
-        #n4j_g = nxneo4j.Graph(self.driver)
-
-        # Create an empty NetworkX graph
-        #nx_g = nx.graph()
 
         # Fetch all triples
         self.template = queries.FETCH_ALL_TRIPLES
         self.query = self.template.format()
 
-        print("Fetching all triples for named individuals - this may take a while...")
+        print("Fetching all triples for named individuals - this may take a"
+              "while...")
         with Spinner():
             query_response = self.run_query_in_session(self.query)
 
         print("All triples received, now processing them...")
         edges = []
-        #edge_labels = []
+
         for triple in query_response:
             n = triple['n'].get('uri').split('#')[-1]
             r = triple['type(r)']
             m = triple['m'].get('uri').split('#')[-1]
-            edges.append((n,r,m))
-            #edge_labels.append(r)
+            edges.append((n, r, m))
 
         G = nx.DiGraph()
-        #G.add_edges_from(edges)
 
         for n, r, m in edges:
             G.add_edge(u_of_edge=n, v_of_edge=m, edge_label=r)
 
-        #return (G, edges, edge_labels)
         return(G)
 
     def to_graphml(self, G=None, edges=None, edge_labels=None):
         if not all([G, edges, edge_labels]):
-            print("Incomplete network data given; calculating via to_networkx_graph()...")
+            print("Incomplete network data given; calculating via"
+                  "to_networkx_graph()...")
             G, edges, edge_labels = self.to_networkx_graph()
             print("...done")
-
-        
-
 
     def build_adjacency_matrix(self, sparse=True):
         """Construct an adjacency matrix of individuals in the
@@ -307,7 +301,7 @@ class Graph:
     def aop_shortest_path(self, mie_node: str, ao_node: str):
         """Find the shortest path between an MIE and an adverse
         outcome using the Neo4j representation of the CO's knowledge
-        base. 
+        base.
 
         Parameters
         ----------
@@ -319,7 +313,7 @@ class Graph:
         query_response = None
 
         if self._validate_connection_status():
-            
+
             self.template = queries.MIE_DISEASE_PATH
             self.query = self.template.format(mie_node, ao_node)
 
@@ -336,17 +330,18 @@ class Graph:
         return(shortest_path)
 
     def page_rank(self, node_type=None):
-        """Compute page rank of all owl__NamedIndividual nodes in the attached graph.
+        """Compute page rank of all owl__NamedIndividual nodes in the attached
+        graph.
 
-        Page rank uses eigenvector centrality to determine "importance" of individual nodes based
-        on the centrality of immediate neighbors.
-        
+        Page rank uses eigenvector centrality to determine "importance" of
+        individual nodes based on the centrality of immediate neighbors.
+
         Parameters
         ----------
         node_type : str, optional
-            Ontology class of desired nodes. Page rank will be computed over all node types, but
-            only the desired type will be returned to the user. If None, no filtering will be
-            performed.
+            Ontology class of desired nodes. Page rank will be computed over
+            all node types, but only the desired type will be returned to the
+            user. If None, no filtering will be performed.
 
         Returns
         -------
@@ -387,7 +382,7 @@ class Graph:
 class Path(object):
     def __init__(self, node_list):
         assert len(node_list) >= 1
-        
+
         self.nodes = node_list
 
         self.start_node = self.nodes[0]

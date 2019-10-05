66745d14fe4dd39c9a76f7606f012a0884cad449
Joseph Romano <jdromano2@gmail.com>
2019-10-05 10:53:15 -0400

    Single-source the package version string
    Former-commit-id: dcf4a2b92a3bf0809598704f72327f6c8c013ea9

--- comptox_ai/__init__.py
+++ comptox_ai/__init__.py
@@ -3,9 +3,19 @@ ComptoxAI
 =========
 """
 
+import os
+from pathlib import Path
+
 from .comptox_ai import ComptoxAI
 from .graph import Graph
 from .ontology import ComptoxOntology
 from .feature_sets import FeatureSet
 
 from . import cypher
+
+package_src_dir = Path(__file__).parent.parent
+
+version_file = open(os.path.join(package_src_dir, 'VERSION'), 'r')
+str_version = version_file.read().strip()
+
+__version__ = str_version

8e9a9efd2b363bda70569cd09e804263625e10a9
Joseph D. Romano <jdromano2@gmail.com>
2019-11-23 00:52:20 -0500

    Move version number into comptox_ai/__init__.py
    Former-commit-id: 6451d999532c79c2ad7c392d3f2c54f9769b7a09

--- comptox_ai/__init__.py
+++ comptox_ai/__init__.py
@@ -15,9 +15,28 @@ from . import aop
 
 from . import cypher
 
-package_src_dir = Path(__file__).parent.parent
+### THIS BREAKS ON SOME BUILD SYSTEMS (e.g., TravisCI):
+# package_src_dir = Path(__file__).parent.parent
+# version_file = open(os.path.join(package_src_dir, 'VERSION'), 'r')
+# str_version = version_file.read().strip()
+##__version__ = str_version
 
-version_file = open(os.path.join(package_src_dir, 'VERSION'), 'r')
-str_version = version_file.read().strip()
-
-__version__ = str_version
+### INSTEAD:
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+#   X.Y
+#   X.Y.Z   # For bugfix releases
+#
+# Admissible pre-release markers:
+#   X.YaN   # Alpha release
+#   X.YbN   # Beta release
+#   X.YrcN  # Release Candidate
+#   X.Y     # Final release
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
+#
+__version__ = '0.1.dev0'
+## ^^ Will be in dev on master branch until 0.1a is ready to go

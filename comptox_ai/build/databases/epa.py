a46b2be83f7df96bd73f0b7b596758d97d72a7d4
Joseph D. Romano <jdromano2@gmail.com>
2019-12-17 16:35:54 -0500

    Move `build` out of `scripts`; add `scr` init param where missing
    Former-commit-id: 16d8208c6aba3d5680d1c76dfc3aebcaa7d53030

--- comptox_ai/build/databases/epa.py
+++ comptox_ai/build/databases/epa.py
@@ -3,6 +3,6 @@ from .hetionet import Hetionet
 from .ctd import CTD
 
 class EPA(Database):
-    def __init__(self, path_or_file="/data1/chemical/epa", name="EPA"):
+    def __init__(self, scr, path_or_file="/data1/chemical/epa", name="EPA"):
         super().__init__(name, path_or_file)
         self.requires = [Hetionet, CTD]

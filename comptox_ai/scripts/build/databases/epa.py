1c21691cd266118eb205069f2bba24059ba23582
Joseph Romano <jdromano2@gmail.com>
2019-12-18 10:57:52 -0500

    Move build files up one level

--- comptox_ai/scripts/build/databases/epa.py
+++
@@ -1,8 +0,0 @@
-from .databases import Database
-from .hetionet import Hetionet
-from .ctd import CTD
-
-class EPA(Database):
-    def __init__(self, path_or_file="/data1/chemical/epa", name="EPA"):
-        super().__init__(name, path_or_file)
-        self.requires = [Hetionet, CTD]

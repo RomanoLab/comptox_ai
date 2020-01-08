b289a60d098cd1b0f720cfac093b00cb0f65b791
Joseph Romano <jdromano2@gmail.com>
2020-01-08 13:45:26 -0500

    Add AOP subgraph builder algorithm.
    This algorithm accepts an AOP title, which it uses to retrieve the
    database node from the graph database. This node is used to walk a subgraph
    using a depth-first approach, constructing a subnetwork bounded by AOP-related
    edge types (e.g., Neo4j edge 'labels'). Documentation will be provided in
    the near future describing the algorithm formally.
    
    
    Former-commit-id: 64fa34e80da66d748dfa35ddeebc744423f08fd2

---
+++ comptox_ai/graph/subgraph.py
@@ -0,0 +1,11 @@
+import networkx as nx
+
+from .graph import Graph
+
+
+class Subgraph(Graph):
+    """
+    A graph that consists of a subset of ComptoxAI's full graph database.
+    """
+    def __init__(self, nodes, edges, nx):
+        pass

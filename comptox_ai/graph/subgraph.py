d830c05eb602ff01322aa22f6ebc5b190ddd6cc2
Joseph D. Romano <jdromano2@gmail.com>
2020-01-09 16:47:31 -0500

    Formalize subgraph.
    Begin to build out the implementation of subgraphs. An important
    characteristic is that they are *not* a descendant of Graph. However,
    they are always derived from a currently instantiated graph. Also, they
    are implemented as a view into the graph from which they are derived,
    meaning data is NOT copied (a substantial advantage)
    
    
    Former-commit-id: 273bd56e87f9296b23f7386ae46458263c34621d

--- comptox_ai/graph/subgraph.py
+++ comptox_ai/graph/subgraph.py
@@ -1,11 +1,20 @@
-import networkx as nx
-
-from .graph import Graph
+import networkx
 
 
-class Subgraph(Graph):
+class Subgraph():
     """
     A graph that consists of a subset of ComptoxAI's full graph database.
     """
-    def __init__(self, nodes, edges, nx):
-        pass
+    def __init__(self, parent_graph, nodes: list = None, from_nx_subgraph: networkx.classes.Graph = None):
+        # super().__init__(driver=parent_graph.driver, build_nx_graph=False)
+
+        self.parent_graph = parent_graph
+
+        if nodes and from_nx_subgraph:
+            raise AttributeError("Cannot pass both `nodes` and `from_subgraph` - please try again.")
+        elif nodes:
+            self.nodes = nodes
+            self.nx = parent_graph.nx.subgraph(self.nodes)
+        elif from_nx_subgraph:
+            self.nodes = list(from_nx_subgraph.nodes())
+            self.nx = from_nx_subgraph

8d05cef6809c36ccf7a2b8ec035d079e6e4744a1
Joseph Romano <jdromano2@gmail.com>
2020-02-03 13:32:24 -0500

    Refactor Graph interface (GraphSAGE format largely functional)
    Former-commit-id: 6cc756c6645d9b724160eec4d3bb6c174e4aac40

---
+++ comptox_ai/graph/utils.py
@@ -0,0 +1,40 @@
+# for Spinner
+import sys
+import time
+import threading
+
+class Spinner(object):
+    """See https://stackoverflow.com/a/39504463/1730417
+    """
+
+    busy = False
+    delay = 0.1
+
+    @staticmethod
+    def spinning_cursor():
+        while 1:
+            for cursor in "|/-\\":
+                yield cursor
+
+    def __init__(self, delay=None):
+        self.spinner_generator = self.spinning_cursor()
+        if delay and float(delay):
+            self.delay = delay
+
+    def spinner_task(self):
+        while self.busy:
+            sys.stdout.write(next(self.spinner_generator))
+            sys.stdout.flush()
+            time.sleep(self.delay)
+            sys.stdout.write("\b")
+            sys.stdout.flush()
+
+    def __enter__(self):
+        self.busy = True
+        threading.Thread(target=self.spinner_task).start()
+
+    def __exit__(self, exception, value, tb):
+        self.busy = False
+        time.sleep(self.delay)
+        if exception is not None:
+            return False

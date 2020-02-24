b5937a40b5a2ab8a1f7e4de4203e30717f41b5a0
Joseph D. Romano <jdromano2@gmail.com>
2020-02-24 00:48:28 -0500

    Switch to new docs structure inspired by sklearn

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,17 +4,6 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
 # -- Project information -----------------------------------------------------
 
 project = 'ComptoxAI'
@@ -23,7 +12,6 @@ author = 'Joseph D. Romano'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1a'
-
 
 # -- General configuration ---------------------------------------------------
 
@@ -36,37 +24,24 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
+templates_path = ['templates']
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = 'alabaster'
+html_theme = 'comptox-ai'
+html_theme_path = ['themes']
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', '_static/css']
+html_static_path = ['_static', '_static/img']
 
 html_favicon = '_static/img/favicon.ico'
 
+html_short_title = 'comptox-ai'
+
 html_theme_options = {
-    'github_user': 'jdromano2',
-    'github_repo': 'comptox_ai',
-    'fixed_sidebar': True,
-    'badge_branch': 'master',
-    'github_button': True,
-    'show_powered_by': False,
-    'font_family': "'Open Sans', sans-serif",
-    'head_font_family': "'Roboto', sans-serif",
-    'font_size': '16px',
-    'page_width': '1140px'
+    'google_analytics': True,
 }

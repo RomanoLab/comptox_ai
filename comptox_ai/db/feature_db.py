6b889755a871096683702109b84fea55c9c9a720
Joseph D. Romano <jdromano2@gmail.com>
2020-08-26 16:28:25 -0400

    Add basic support for feature db client.

--- comptox_ai/db/feature_db.py
+++ comptox_ai/db/feature_db.py
@@ -1,5 +1,16 @@
+"""
+Document store interface.
+
+Copyright (c) 2020 by Joseph D. Romano
+"""
+
 import os
+import glob
+import configparser
+import urllib.parse
 from pathlib import Path
+
+from pymongo import MongoClient
 
 ROOT_DIR = Path(__file__).resolve().parents[2]
 if os.path.exists(os.path.join(ROOT_DIR, 'CONFIG.cfg')):
@@ -8,16 +19,48 @@ else:
   DEFAULT_CONFIG_FILE = os.path.join(ROOT_DIR, 'CONFIG-default.cfg')
 
 class FeatureDB(object):
+  """
+  A database of feature data for entities in ComptoxAI, implemented as a
+  document store in MongoDB.
+
+  Parameters
+  ----------
+  config_file : str, default None
+    Relative path to a config file containing a "NEO4J" block, as described
+    below. If None, ComptoxAI will look in the ComptoxAI root directory for
+    either a "CONFIG.cfg" file or "CONFIG-default.cfg", in that order. If no
+    config file can be found in any of those locations, an exception will be
+    raised.
+  verbose: bool, default True
+    Sets verbosity to on or off. If True, status information will be returned
+    to the user occasionally.
+  """
+
   def __init__(self, config_file=None, verbose=True):
+    self.verbose = verbose
+    
     if not config_file:
       self.config_file = DEFAULT_CONFIG_FILE
     else:
       self.config_file = config_file
 
-    self._parse_config()
+    self._connect()
 
-  def _parse_config():
+  def _connect(self):
     assert self.config_file is not None
+
+    cnf = configparser.ConfigParser()
+    cnf.read(self.config_file)
+    
+    username = urllib.parse.quote_plus(cnf["MONGODB"]["Username"])
+    password = urllib.parse.quote_plus(cnf["MONGODB"]["Password"])
+    
+    if (len(username) > 0) and (len(password) > 0):
+      uri = "mongodb://{0}:{1}@127.0.0.1".format(username, password)
+    else:
+      uri = "mongodb://127.0.0.1"
+
+    self._client = MongoClient(uri)
 
   def fetch(self):
     pass

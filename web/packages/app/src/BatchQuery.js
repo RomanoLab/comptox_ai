9cec915a559d3d494b7609f8f40d3c9564789944
Joseph D. Romano <jdromano2@gmail.com>
2021-04-17 17:01:07 -0400

    Split components into their own files and start mockup of RelationshipSearch
    Former-commit-id: 32ca5f288e03dd819f9a51c008a3c3dbeb031cb9

---
+++ web/packages/app/src/BatchQuery.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class BatchQuery extends React.Component {
+  render() {
+    return(
+      <div>
+        <h2>Bulk data fetch</h2>
+        <p>
+          This tool is used to retrieve node and/or relationship properties for many nodes or relationships simultaneously.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default BatchQuery;

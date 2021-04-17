8476e9926d225f40bee013b69d71924ca25c0e71
Joseph Romano <jdromano2@gmail.com>
2021-04-17 11:59:31 -0400

    Start to add dynamic components to data page
    Former-commit-id: d05ab744b1380f19173e598fc4497ced86866cfc

--- web/packages/app/src/app/store.js
+++ web/packages/app/src/app/store.js
@@ -1,8 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
   },
 });

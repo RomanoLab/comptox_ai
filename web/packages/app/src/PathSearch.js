982b1c97681ad77ab142d5158f886567a7af43ca
Joseph Romano <jdromano2@gmail.com>
2021-04-21 09:48:56 -0400

    Make basic fields for path search
    Former-commit-id: 42db0884629a06941874dd558fc44b395c306c74

--- web/packages/app/src/PathSearch.js
+++ web/packages/app/src/PathSearch.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
 class PathSearch extends React.Component {
   render() {
@@ -8,6 +9,18 @@ class PathSearch extends React.Component {
         <p>
           <i>Paths are chains of two or more nodes in the graph database along with the relationships that link them.</i>
         </p>
+        <TextField
+          id="startNodeValue"
+          label="Start node"
+          variant="outlined"
+          style={{ width: 500, paddingBottom: 8 }}
+        />
+        <TextField
+          id="endNodeValue"
+          label="End node"
+          variant="outlined"
+          style={{ width: 500, paddingBottom: 8 }}
+        />
       </div>
     );
   }

0dea3f0aa46770b200b80e8c004adb47b0881b7d
Joseph D. Romano <jdromano2@gmail.com>
2021-06-30 00:21:49 -0400

    Tweak README and remove dead files

--- comptox_ai/build/databases/__init__.py
+++
@@ -1,6 +0,0 @@
-from .databases import Database
-
-from .hetionet import Hetionet
-from .ctd import CTD
-from .epa import EPA
-from .aopwiki import AOPWiki
--- comptox_ai/build/mapping_functions.py
+++
@@ -1,6 +0,0 @@
-"""
-Functions for mapping identifiers across databases and terminologies.
-
-Copyright (c) 2020 by Joseph D. Romano
-"""
-

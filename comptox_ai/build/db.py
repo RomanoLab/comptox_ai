f40be64d7b7033d1203125a3beb1327f45ec12ad
Joseph D. Romano <jdromano2@gmail.com>
2021-11-10 00:35:40 -0500

    Add chemical-gene ixns
    Former-commit-id: c33fca1d54d43f70144e9a58a984c398e18671b6

--- comptox_ai/build/db.py
+++ comptox_ai/build/db.py
@@ -710,6 +710,9 @@ def print_ontology_stats(ont: owlready2.namespace.Ontology):
 
 
 if __name__ == "__main__":
+
+    WARM_START = False
+
     repo_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
     onto = owlready2.get_ontology("file://{}".format(os.path.join(repo_root, "comptox.rdf"))).load()
     #onto = owlready2.get_ontology("file://{}".format(os.path.join(repo_root, "comptox_mid.rdf"))).load()
@@ -738,111 +741,144 @@ if __name__ == "__main__":
 
     # Add nodes and node properties in a carefully specified order
 
-    #####################
-    # EPA COMPTOX NODES #
-    #####################
-    epa.parse_node_type(
-        node_type="Chemical",
-        source_filename="PubChem_DTXSID_mapping_file.txt",
-        fmt="tsv",
-        parse_config={
-            "iri_column_name": "DTXSID",
-            "headers": True,
-            "data_property_map": {
-                "CID": onto.xrefPubchemCID,
-                "SID": onto.xrefPubchemSID,
-                "DTXSID": onto.xrefDTXSID,
+    if WARM_START:
+        onto = owlready2.get_ontology("file://{}".format(os.path.join(repo_root, "comptox_mid.rdf"))).load()
+    else:
+        #####################
+        # EPA COMPTOX NODES #
+        #####################
+        epa.parse_node_type(
+            node_type="Chemical",
+            source_filename="PubChem_DTXSID_mapping_file.txt",
+            fmt="tsv",
+            parse_config={
+                "iri_column_name": "DTXSID",
+                "headers": True,
+                "data_property_map": {
+                    "CID": onto.xrefPubchemCID,
+                    "SID": onto.xrefPubchemSID,
+                    "DTXSID": onto.xrefDTXSID,
+                },
             },
-        },
-        merge=False,
-        skip=False
-    )
-    epa.parse_node_type(
-        node_type="Chemical",
-        source_filename="Dsstox_CAS_number_name.csv",
-        fmt="csv",
-        parse_config={
-            "iri_column_name": "dsstox_substance_id",
-            "headers": True,
-            "data_property_map": {
-                "casrn": onto.xrefCasRN,
-                "preferred_name": onto.commonName,
-                "dsstox_substance_id": onto.xrefDTXSID,
+            merge=False,
+            skip=False
+        )
+        epa.parse_node_type(
+            node_type="Chemical",
+            source_filename="Dsstox_CAS_number_name.csv",
+            fmt="csv",
+            parse_config={
+                "iri_column_name": "dsstox_substance_id",
+                "headers": True,
+                "data_property_map": {
+                    "casrn": onto.xrefCasRN,
+                    "preferred_name": onto.commonName,
+                    "dsstox_substance_id": onto.xrefDTXSID,
+                },
+                "merge_column": {
+                    "source_column_name": "dsstox_substance_id",
+                    "data_property": onto.xrefDTXSID,
+                },
             },
-            "merge_column": {
-                "source_column_name": "dsstox_substance_id",
-                "data_property": onto.xrefDTXSID,
+            merge=True,
+            skip=False
+        )
+        # with open("D:\\projects\\comptox_ai\\comptox_mid.rdf", "wb") as fp:
+        #     onto.save(file=fp, format="rdfxml")
+        # ipdb.set_trace()
+        # epa.parse_node_type(
+        #     node_type="Chemical",
+        #     source_filename="CUSTOM_cid_maccs.tsv",
+        #     fmt="tsv",
+        #     parse_config={
+        #         "iri_column_name": "CID",
+        #         "headers": True,
+        #         "data_property_map": {
+        #             "CID": onto.xrefPubchemCID,
+        #             "MACCS": onto.maccs
+        #         },
+        #         "merge_column": {
+        #             "source_column_name": "CID",
+        #             "data_property": onto.xrefPubchemCID
+        #         },
+        #     },
+        #     merge=True,
+        #     skip_create_new_node=True,  # Don't create an empty chemical node with just a MACCS property if the CID isn't already in the ontology
+        #     skip=False
+        # )
+        epa.parse_node_type(
+            node_type="Chemical",
+            source_filename="CUSTOM/chemical_maccs_fingerprints.tsv",
+            fmt="tsv",
+            parse_config={
+                "iri_column_name": "DTXSID",
+                "headers": True,
+                "data_property_map": {
+                    "DTXSID": onto.xrefDTXSID,
+                    "MACCS": onto.maccs
+                },
+                "merge_column": {
+                    "source_column_name": "DTXSID",
+                    "data_property": onto.xrefDTXSID
+                }
             },
-        },
-        merge=True,
-        skip=False
-    )
-    # with open("D:\\projects\\comptox_ai\\comptox_mid.rdf", "wb") as fp:
-    #     onto.save(file=fp, format="rdfxml")
-    # ipdb.set_trace()
-    # epa.parse_node_type(
-    #     node_type="Chemical",
-    #     source_filename="CUSTOM_cid_maccs.tsv",
-    #     fmt="tsv",
-    #     parse_config={
-    #         "iri_column_name": "CID",
-    #         "headers": True,
-    #         "data_property_map": {
-    #             "CID": onto.xrefPubchemCID,
-    #             "MACCS": onto.maccs
-    #         },
-    #         "merge_column": {
-    #             "source_column_name": "CID",
-    #             "data_property": onto.xrefPubchemCID
-    #         },
-    #     },
-    #     merge=True,
-    #     skip_create_new_node=True,  # Don't create an empty chemical node with just a MACCS property if the CID isn't already in the ontology
-    #     skip=False
-    # )
-    epa.parse_node_type(
-        node_type="Chemical",
-        source_filename="CUSTOM/chemical_maccs_fingerprints.tsv",
-        fmt="tsv",
-        parse_config={
-            "iri_column_name": "DTXSID",
-            "headers": True,
-            "data_property_map": {
-                "DTXSID": onto.xrefDTXSID,
-                "MACCS": onto.maccs
-            },
-            "merge_column": {
-                "source_column_name": "DTXSID",
-                "data_property": onto.xrefDTXSID
-            }
-        },
-        merge=True,
-        skip_create_new_node=True,  # Don't create an empty chemical node with just a MACCS property if the CID isn't already in the ontology
-        skip=False
-    )
+            merge=True,
+            skip_create_new_node=True,  # Don't create an empty chemical node with just a MACCS property if the CID isn't already in the ontology
+            skip=False
+        )
 
-    ##################
-    # CHEMICAL LISTS #
-    ##################
-    epa.parse_node_type(
-        node_type="ChemicalList",
-        source_filename="CUSTOM/Chemical Lists.tsv",
-        fmt="tsv",
-        parse_config={
-            "iri_column_name": "LIST_ACRONYM",
-            "headers": True,
-            "data_property_map": {
-                "LIST_ACRONYM": onto.listAcronym,
-                "LIST_NAME": onto.commonName,
-                "LIST_DESCRIPTION": onto.listDescription
+        ##################
+        # CHEMICAL LISTS #
+        ##################
+        epa.parse_node_type(
+            node_type="ChemicalList",
+            source_filename="CUSTOM/Chemical Lists.tsv",
+            fmt="tsv",
+            parse_config={
+                "iri_column_name": "LIST_ACRONYM",
+                "headers": True,
+                "data_property_map": {
+                    "LIST_ACRONYM": onto.listAcronym,
+                    "LIST_NAME": onto.commonName,
+                    "LIST_DESCRIPTION": onto.listDescription
+                },
+                "data_transforms": {
+                    "LIST_ACRONYM": lambda x: x.split('/')[-1]
+                }
             },
-            "data_transforms": {
-                "LIST_ACRONYM": lambda x: x.split('/')[-1]
-            }
-        },
-        merge=False,
-        skip=False
-    )
+            merge=False,
+            skip=False
+        )
+
+        ###############################
+        # Chemical List relationships #
+        ###############################
+        epa.parse_relationship_type(
+            relationship_type=onto.listIncludesChemical,
+            inverse_relationship_type=onto.chemicalInList,
+            source_filename="CUSTOM/chemical_lists_relationships.tsv",
+            fmt="tsv",
+            parse_config = {
+                "subject_node_type": onto.ChemicalList,
+                "subject_column_name": "list_acronym",
+                "subject_match_property": onto.listAcronym,
+                "object_node_type": onto.Chemical,
+                "object_column_name": "casrn",
+                "object_match_property": onto.xrefCasRN,
+                "headers": True
+            },
+            merge=True,
+            skip=False
+        )
+
+        if os.name == 'nt':
+            print("SAVING INITIAL CHEMICAL DATA...")
+            with open("D:\\projects\\comptox_ai\\comptox_mid.rdf", "wb") as fp:
+                onto.save(file=fp, format="rdfxml")
+        else:
+            raise NotImplementedError("Whoops!")
+
+    #onto = owlready2.get_ontology("file://{}".format(os.path.join(repo_root, "comptox_mid.rdf"))).load()
 
     ##################
     # DRUGBANK NODES #
@@ -980,6 +1016,21 @@ if __name__ == "__main__":
         merge=True,
         skip=False
     )
+    # We also want to include AOPDB chemical IDs here (which happen to be MeSH terms)
+    aopdb.parse_node_type(
+        node_type="Chemical",
+        source_table="chemical_info",
+        parse_config={
+            "iri_column_name": "DTX_id",
+            "data_property_map": {"ChemicalID": onto.xrefMeSH},
+            "merge_column": {
+                "source_column_name": "DTX_id",
+                "data_property": onto.xrefDTXSID
+            }
+        },
+        merge=True,
+        skip=False
+    )
     aopdb.parse_node_type(
         node_type="Pathway",
         source_table="stressor_info",
@@ -1051,6 +1102,38 @@ if __name__ == "__main__":
         skip=False
     )
     aopdb.parse_relationship_type(
+        relationship_type=onto.chemicalIncreasesExpression,
+        parse_config = {
+            "subject_node_type": onto.Chemical,
+            "subject_column_name": "DTX_id",
+            "subject_match_property": onto.xrefDTXSID,
+            "object_node_type": onto.Gene,
+            "object_column_name": "entrez",
+            "object_match_property": onto.xrefNcbiGene,
+            "custom_sql_query": "SELECT * FROM aopdb.chemical_gene WHERE tax_id = 9606 AND InteractionActions LIKE '%increases^expression%';",  # only interested in human expression
+            "source_table_type": "foreignKey",
+            "source_table": "chemical_gene",
+        },
+        merge=False,
+        skip=False
+    )
+    aopdb.parse_relationship_type(
+        relationship_type=onto.chemicalDecreasesExpression,
+        parse_config = {
+            "subject_node_type": onto.Chemical,
+            "subject_column_name": "DTX_id",
+            "subject_match_property": onto.xrefDTXSID,
+            "object_node_type": onto.Gene,
+            "object_column_name": "entrez",
+            "object_match_property": onto.xrefNcbiGene,
+            "custom_sql_query": "SELECT * FROM aopdb.chemical_gene WHERE tax_id = 9606 AND InteractionActions LIKE '%decreases^expression%';",  # only interested in human expression
+            "source_table_type": "foreignKey",
+            "source_table": "chemical_gene",
+        },
+        merge=False,
+        skip=False
+    )
+    aopdb.parse_relationship_type(
         relationship_type=onto.keyEventTriggeredBy,
         parse_config = {
             "subject_node_type": onto.KeyEvent,
@@ -1105,7 +1188,7 @@ if __name__ == "__main__":
                 "target": lambda x: int(x.split("::")[-1])  # I foresee this causing problems in the future - should all IDs be cast to str?
             },
         },
-        merge=False,
+        merge=True,  # Merge with aopdb/ctd chemical-gene ixns
         skip=False
     )
     hetionet.parse_relationship_type(
@@ -1127,7 +1210,7 @@ if __name__ == "__main__":
                 "target": lambda x: int(x.split("::")[-1])  # I foresee this causing problems in the future - should all IDs be cast to str?
             },
         },
-        merge=False,
+        merge=True,
         skip=False
     ),
     hetionet.parse_relationship_type(

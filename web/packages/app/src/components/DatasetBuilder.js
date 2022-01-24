0695c169b92092600d59f1dc8287ccfd8731a3c3
Joseph D. Romano <jdromano2@gmail.com>
2022-01-24 00:52:49 -0500

    Build a working QSAR dataset generator interface
    Former-commit-id: 212f6adab0bdbdb4d688cfd272680363528b7f50

--- web/packages/app/src/components/DatasetBuilder.js
+++ web/packages/app/src/components/DatasetBuilder.js
@@ -1,186 +1,148 @@
-import React, { useReducer, useState } from 'react';
-
+import React, { useState } from 'react';
 import {
-  Collapse,
+  Button,
   Container,
   FormControl,
   FormControlLabel,
-  Grid,
-  InputLabel,
-  MenuItem,
-  Select,
   Switch,
   TextField
 } from '@material-ui/core';
 import Autocomplete from '@material-ui/lab/autocomplete';
 
-
 const qsarBuilderConfig = require('../data/qsar_params_data.json');
 
-
-
-const formReducer = (state, event) => {
-  return {
-    ...state,
-    [event.name]: event.value
-  }
-}
-
 const DatasetBuilderQueryForm = (props) => {
-  const { config, chemLists } = props;
 
-  const [formData, setFormData] = useReducer(formReducer, {});
+  const [chemListValue, setChemListValue] = useState({'acronym': '', 'name': ''});
+  const [chemListInputValue, setChemListInputValue] = React.useState('');
 
-  const [checked, setChecked] = React.useState(false);
+  const [assayValue, setAssayValue] = useState({'assayId': '', 'assayName': ''});
+  const [assayInputValue, setAssayInputValue] = React.useState('');
 
-  //const chemicalFields = config.nodeConfig.nodeLabelProperties['Chemical'];
-  const chemicalFields = [
-    {
-      "property": "commonName",
-      "display": "Chemical Name"
-    },
-    {
-      "property": "uri",
-      "display": "Ontology URI"
-    },
-    {
-      "property": "xrefCasRN",
-      "display": "CAS Registry Number"
-    },
-    {
-      "property": "xrefDTXSID",
-      "display": "DSSTOX ID"
-    },
-    {
-      "property": "xrefPubchemCID",
-      "display": "PubChem CID"
-    },
-    {
-      "property": "xrefPubchemSID",
-      "display": "PubChem SID"
-    }
-  ];
-
-  const handleChange = event => {
-    console.log(event);
-    setFormData({
-      name: event.target.name,
-      value: event.target.value,
-    });
-  }
-  
   const handleSubmit = event => {
     event.preventDefault();
+    // Open new tab with API URL
+    window.open(`http://localhost:3000/datasets/makeQsarDataset?assay=${assayValue.assayId}&chemList=${chemListValue.acronym}`);
   }
-
-  const handleToggleChemFilter = () => {
-    setChecked((prev) => !prev);
-  };
 
   return (
     <form onSubmit={handleSubmit}>
 
       <Container>
-        <FormControlLabel
-          control={<Switch color="primary" checked={checked} onChange={handleToggleChemFilter} />}
-          label="Filter Chemicals"
-          style={{ paddingBottom: 8 }}
-        />
-        <Collapse in={checked}>
 
-          <Grid container spacing={3} style={{ paddingBottom: 8 }}>
-            <Grid item xs={2}>
-              <FormControl
-                variant="outlined"
-                size="small"
-                // style={{ width: '20%', paddingBottom: 8, paddingRight: 8 }}
-                style={{ width: '100%' }}
+        {/* <Grid container spacing={3} style={{ paddingBottom: 8 }}>
+          <Grid item xs={2}>
+            <FormControl
+              variant="outlined"
+              size="small"
+              // style={{ width: '20%', paddingBottom: 8, paddingRight: 8 }}
+              style={{ width: '100%' }}
+            >
+              <InputLabel id="select-outlined-label-filter-prop">Filter by...</InputLabel>
+              <Select
+                name="chemicalFilterPropertySelect"
+                label="Filter by..."
+                labelId="select-outlined-label-filter-prop"
+                onChange={handleChange}
+                value={formData.chemicalFilterPropertySelect || ''}
+                defaultValue=""
               >
-                <InputLabel id="select-outlined-label-filter-prop">Filter by...</InputLabel>
-                <Select
-                  name="chemicalFilterPropertySelect"
-                  label="Filter by..."
-                  labelId="select-outlined-label-filter-prop"
-                  onChange={handleChange}
-                  value={formData.chemicalFilterPropertySelect || ''}
-                  defaultValue=""
-                >
-                  {chemicalFields.map((cf) => (
-                    <MenuItem value={cf.property}>{cf.display}</MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </Grid>
-
-            <Grid item xs={1}>
-              <FormControl
-                variant="outlined"
-                size="small"
-                style={{ width: '100%' }}
-              >
-                <Select
-                  defaultValue="="
-                >
-                  <MenuItem value="=">=</MenuItem>
-                  <MenuItem value="contains">contains</MenuItem>
-                  <MenuItem value="not">not</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-
-            <Grid item xs={9}>
-              <FormControl
-                variant="outlined"
-                size="small"
-                style={{ width: '100%' }}
-              >
-                {/* <InputLabel id="select-outlined-label-filter-value">Filter value</InputLabel> */}
-                <TextField
-                  name="chemicalFilterValue"
-                  label="Filter value"
-                  labelId="select-outlined-label-filter-value"
-                  onChange={handleChange}
-                  value={formData.chemicalFilterValue || ''}
-                  defaultValue=""
-                  variant="outlined"
-                  size="small"
-                />
-              </FormControl>
-            </Grid>
+                {chemicalFields.map((cf) => (
+                  <MenuItem value={cf.property}>{cf.display}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
           </Grid>
 
-
-          <FormControl
-            variant="outlined"
-            size="small"
-            style={{ width: '100%', paddingBottom: 8 }}
-          >
-            <Autocomplete
-              id="autocomplete-outlined-chemicallist"
-              // options={chemLists}
+          <Grid item xs={1}>
+            <FormControl
+              variant="outlined"
               size="small"
-              options={qsarBuilderConfig.chemicalLists}
-              getOptionLabel={(option) => (option.name + " (n=" + option.num_chems + ")")}
-              renderInput={(params) => <TextField {...params} label="EPA Chemical List filter" variant="outlined" />}
-            />
-          </FormControl>
+              style={{ width: '100%' }}
+            >
+              <Select
+                defaultValue="="
+              >
+                <MenuItem value="=">=</MenuItem>
+                <MenuItem value="contains">contains</MenuItem>
+                <MenuItem value="not">not</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
 
-          <FormControl
-            variant="outlined"
-            size="small"
-            style={{ width: '100%'}}
-          >
-            <Autocomplete
-              id="autocomplete-outlined-assayendpoint"
-              // options={chemLists}
+          <Grid item xs={9}>
+            <FormControl
+              variant="outlined"
               size="small"
-              options={qsarBuilderConfig.assays}
-              getOptionLabel={(option) => (option.assayName + " (id: " + option.assayId + ")")}
-              renderInput={(params) => <TextField {...params} label="Assay endpoint for QSAR" variant="outlined" />}
-            />
-          </FormControl>
+              style={{ width: '100%' }}
+            >
+              <InputLabel id="select-outlined-label-filter-value">Filter value</InputLabel>
+              <TextField
+                name="chemicalFilterValue"
+                label="Filter value"
+                labelId="select-outlined-label-filter-value"
+                onChange={handleChange}
+                value={formData.chemicalFilterValue || ''}
+                defaultValue=""
+                variant="outlined"
+                size="small"
+              />
+            </FormControl>
+          </Grid>
+        </Grid> */}
 
-        </Collapse>
+
+        <FormControl
+          variant="outlined"
+          size="small"
+          style={{ width: '100%', paddingBottom: 8 }}
+        >
+          <Autocomplete
+            id="autocomplete-outlined-chemicallist"
+            // options={chemLists}
+            size="small"
+            onChange={(event, newValue) => {
+              setChemListValue(newValue);
+            }}
+            inputValue={chemListInputValue}
+            onInputChange={(event, newInputValue) => {
+              setChemListInputValue(newInputValue);
+            }}
+            value={chemListValue || ''}
+            options={qsarBuilderConfig.chemicalLists}
+            getOptionLabel={(option) => (option.name + " (n=" + option.num_chems + ")")}
+            getOptionSelected={(option, value) => option.acronym === value.acronym}
+            renderInput={(params) => <TextField {...params} label="EPA Chemical List filter" variant="outlined" />}
+          />
+        </FormControl>
+
+        <FormControl
+          variant="outlined"
+          size="small"
+          style={{ width: '100%', paddingBottom: 8 }}
+        >
+          <Autocomplete
+            id="autocomplete-outlined-assayendpoint"
+            size="small"
+            onChange={(event, newValue) => {
+              setAssayValue(newValue);
+            }}
+            inputValue={assayInputValue}
+            onInputChange={(event, newInputValue) => {
+              setAssayInputValue(newInputValue)
+            }}
+            value={assayValue || ''}
+            options={qsarBuilderConfig.assays}
+            getOptionLabel={(option) => (option.assayName + " (id: " + option.assayId + ")")}
+            getOptionSelected={(option, value) => option.assayId === value.assayId}
+            renderInput={(params) => <TextField {...params} label="Assay endpoint for QSAR" variant="outlined" />}
+          />
+        </FormControl>
+
+        <br/>
+        <Button variant="contained" color="primary" type="submit" style={{marginRight: 6}}>Build QSAR Dataset</Button>
+        <i>(Results will open in a new tab)</i>
+
       </Container>
     </form>
   )
@@ -205,10 +167,6 @@ const DatasetBuilder = (props) => {
           />
 
         </div>
-      </div>
-      <div className="datasetBuilderResults">
-        <h3>Download available dataset</h3>
-        <p><i>Datasets will be available to download for 1 hour after creation.</i></p>
       </div>
     </div>
   );

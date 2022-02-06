7ef67f7f0776ec84389f163484c88dedcc649f9b
Joseph D. Romano <jdromano2@gmail.com>
2022-02-06 10:05:13 -0500

    Incremental work on Graphs

---
+++ web/packages/api/models/graphs.js
@@ -0,0 +1,8 @@
+const Graph = require('./neo4j/graph');
+
+// Run an arbitrary Cypher query and return a Graph
+const getGraphCypher = function () {
+    return new Promise( (resolve, reject) => {
+        
+    })
+}
---
+++ web/packages/api/models/neo4j/graph.js
@@ -0,0 +1,5 @@
+const neo4j = require('neo4j-driver');
+
+const Graph = module.exports = function(_graph, graphData) {
+
+}
